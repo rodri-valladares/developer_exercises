93d8956a52ef8bc607a23397810167b26c116f9b
rodriV <utndavid@gmail.com>
2022-11-16 09:01:51 -0300

    Agrega doctest a ejercicio [simple]

--- simple/simple.py
+++ simple/simple.py
@@ -2,6 +2,20 @@ import random
 
 
 def genera_lista():
+    """Función que retorno una lista de diccionarios de edades 
+
+    Returns:
+        list_edades: lista de diccionarios de longitud = 10
+
+    >>> len(genera_lista()) == 10
+    True
+
+    >>> list_example = genera_lista()
+    >>> type(list_example[0])
+    <class 'dict'>
+    """
+    
+  
     list_edades = []
 
     for i in range(10):
@@ -14,6 +28,22 @@ def genera_lista():
 
 
 def ordenar_lista(list_edades):
+    """Función que ordena una lista de diccionarios segun su edad.
+    
+
+    Args:
+        list_edades (list):lista a ordenar
+
+    Returns:
+        list_ordenada: lista ordenada según su edad 
+
+    >>> list_example = [{'id': 0, 'edad': 37}, {'id': 1, 'edad': 18}, {'id': 2, 'edad': 77}, {'id': 3, 'edad': 50}, {'id': 4, 'edad': 83}, {'id': 5, 'edad': 55}, {'id': 6, 'edad': 8}, {'id': 7, 'edad': 75}, {'id': 8, 'edad': 89}, {'id': 9, 'edad': 73}]
+    >>> list_ordenada = ordenar_lista(list_example)
+    >>> list_ordenada[0]["id"] == 6
+    True
+    >>> list_ordenada[9]["id"] == 8
+    True
+    """
 
     list_ordenada = sorted(list_edades, key=lambda d: d['edad'])
 
